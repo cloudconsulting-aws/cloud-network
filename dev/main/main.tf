a38c5b328a72523e4a81121b095c308d4d66cebe
Asaph Tinoco <asaph@sendwave.com>
2025-07-01 19:02:37 +0200

    changing numbers of subnet

--- dev/main/main.tf
+++ dev/main/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
   source                  = "git::https://github.com/cloudconsulting-aws/terraform-aws-vpc.git?ref=v1.1.0"
   vpc_cidr_block          = "10.0.0.0/16"
   public_subnet_count     = 2
-  private_subnet_count    = 3
+  private_subnet_count    = 2
   ips_per_subnet_exponent = 8
   project_name            = "main-project"
   availability_zones      = data.aws_availability_zones.azs.names
